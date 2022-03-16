f73d6a79173fb1d1229bcda92cb543fbcd67f3f7
101-Don <eugineotiende61498@gmail.com>
2022-03-16 12:13:17 -0700

    puchat

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
-*prints _putchar
-*followed by a new line
+* main -  prints Holberton, followed by a new line
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-printf("_putchar\n");
-return 0;
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
 }
-
